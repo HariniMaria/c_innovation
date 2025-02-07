5c1e23592cfd1c7320f188c0e9721b3b0bf5ab1b
Harini Maria G <127276464+HariniMaria@users.noreply.github.com>
2025-02-07 14:05:24 +0530

    Update power.c

--- power.c
+++ power.c
@@ -3,18 +3,18 @@
 int main() {
     int n, power;
     long long result = 1; 
-    // Getting input from user
+    
     printf("Enter the base (n): ");
     scanf("%d", &n);
     printf("Enter the exponent (power): ");
     scanf("%d", &power);
 
-    // Calculate power using a loop
+    
     for (int i = 0; i < power; i++) {
         result *= n;
     }
 
-    // Display result
+    
     printf("%d^%d = %lld\n", n, power, result);
 
     return 0;
